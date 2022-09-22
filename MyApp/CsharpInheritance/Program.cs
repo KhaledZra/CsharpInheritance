426f0fb0ad2c334758c58444331aee6b47e4c3ba
Khaled Zra <halo_434000@hotmail.com>
2022-09-22 08:18:20 +0200

    Setting up

---
+++ MyApp/CsharpInheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
